99cff34826e51d384819def90c91eb6f19b5414e
Mikica Ivosevic <mikica.ivosevic@gmail.com>
2015-11-06 15:11:43 +0100

    Update Generator.php

--- src/QrCodeBitcoin/QrCode/Generator.php
+++ src/QrCodeBitcoin/QrCode/Generator.php
@@ -13,9 +13,9 @@ use Zend\View\Helper\AbstractHelper;
 
 class Generator extends AbstractHelper
 {
-    public function __invoke($address, $amount)
+    public function __invoke($address, $amount, $width=300, $height=300)
     {
-        $image = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcoin:{$address}?amount={$amount}";
+        $image = "https://chart.googleapis.com/chart?chs={$width}x{$height}&cht=qr&chl=bitcoin:{$address}?amount={$amount}";
         $imageFile = file_get_contents($image);
         $imageData = base64_encode($imageFile);
         $src = 'data: '.mime_content_type($image).';base64,'.$imageData;
